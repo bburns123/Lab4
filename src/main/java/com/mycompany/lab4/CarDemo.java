ac6201b64385a4444a68be9266d84aac0e8002bf
Brendyn Burns <Brendyn Burns@DESKTOP-9USEOT4>
2019-10-15 18:02:13 -0500

    CarClass and CarDemo First Commit

---
+++ src/main/java/com/mycompany/lab4/CarDemo.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lab4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Brendyn Burns
+ */
+public class CarDemo {
+    public static void main(String[] args) {
+        CarClass car1 = new CarClass(1993, "Ford");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Press 'Enter' to accelerate 5 times!");
+        scanner.nextLine();
+        
+        for(int counter = 1; counter <=5; counter++) {
+            car1.accelerate();
+            System.out.println("Current speed: " + car1.getSpeed());
+            scanner.nextLine();
+        }
+        
+        for(int counter = 1; counter <=5; counter++) {
+            car1.brake();
+            System.out.println("Current speed: " + car1.getSpeed());
+            scanner.nextLine();
+        }
+    }
+}
